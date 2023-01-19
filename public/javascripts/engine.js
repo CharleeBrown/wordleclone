41ecdd8a88cc25fef934ec7759a6f1b009c47fb1
Coleman <charleebrown22@gmail.com>
2023-01-19 15:37:43 -0600

    the guessing is correct so far.

--- public/javascripts/engine.js
+++ public/javascripts/engine.js
@@ -1,10 +1,20 @@
 var x = 0;
-
+var wordArray = [];
+var guessword = "chess";
+var holdArray = [];
 document.addEventListener("keydown", (e) =>{
-
-	console.log(e.key);
 	x=x+1;
-	if(x<5){
-		alert(e.key + " " + x);
+	if(x<6){
+		console.log(e.key + "- " + x);
+		holdArray.push(e.key);
 	}
-})
+	else if(x=6){
+		console.log(holdArray);
+		if(holdArray.join("") == guessword){
+			console.log("correct");
+		}
+		wordArray.push(holdArray);
+		holdArray = [];
+		x=0;
+	}
+});
