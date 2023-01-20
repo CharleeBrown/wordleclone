969729ff9d8883e9cdbc253951de1b1360e432a9
Coleman <charleebrown22@gmail.com>
2023-01-20 08:23:23 -0600

    I've added backspacing and regex for only alpha ch

--- public/javascripts/engine.js
+++ public/javascripts/engine.js
@@ -3,27 +3,43 @@ var characterCount = 0;
 var wordArray = [];
 var guessword = "chess";
 var holdArray = [];
-
+var inputs = document.getElementById("words");;
+var charRegex = new RegExp(/^[A-Z]+$/i);
 
 // TODO: only allow the event listener once the page has loaded.
 // TODO: add a regex for only alpha characters
-
+inputs.value="";
 // *Listens for a key press
 document.addEventListener("keydown", (e) =>{
 	// *If the count is less than 6
-	if(characterCount<6){
+	if(e.key.match(/^[a-zA-Z]$/)){
+		if(characterCount<6){
 		// *Log the keycode and index of character
 		console.log(e.key + "- " + characterCount);
+	
+		inputs.value += e.key;
 		holdArray.push(e.key);
-		characterCount=+1;
+		characterCount+=1;
 	}
-	else if(x=6){
+	else if(characterCount=6){
 		if(holdArray.join("") == guessword){
-			console.log("correct");
+			alert("correct");
 		}
+		inputs.value = inputs.value + "\n";
 		console.log(holdArray);
 		wordArray.push(holdArray);
 		holdArray = [];
-		x=0;
+		characterCount=0;
 	}
+}
+else if(e.key =="Backspace" && characterCount < 6){
+	characterCount-=1;
+	inputs.value = inputs.value.substring(0, inputs.value.length - 1);
+	holdArray.pop();
+}
+else{
+	e.preventDefault();
+}
 });
+
+
